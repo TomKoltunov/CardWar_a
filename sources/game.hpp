7d98b3a66c0c77948756f55b6a7506fe5afe1a95
TomKoltunov <74180693+TomKoltunov@users.noreply.github.com>
2023-03-27 12:26:36 +0300

    added to headers

--- sources/game.hpp
+++ sources/game.hpp
@@ -3,3 +3,16 @@
 
 using namespace std;
 
+namespace ariel
+{
+    class Game
+    {
+        Game(Player 1, Player 2);
+        void printLastTurn(); // Print the last turn stats
+        void playTurn();
+        void playAll(); // Playes the game untill the end
+        void printWiner(); // Prints the name of the winning player
+        void printLog(); // Prints all the turns played one line per turn (same format as game.printLastTurn())
+        void printStats();// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand (draw within a draw counts as 2 draws)
+    }
+}
--- sources/player.hpp
+++ sources/player.hpp
@@ -8,5 +8,7 @@ namespace ariel
     class Player
     {
         Player(string name);
+        void stacksize(); // Prints the amount of cards left. Should be 21 but can be less if a draw was played
+        void cardesTaken(); // Prints the amount of cards this player has won
     }
 }
